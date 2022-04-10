389255dba4ab993ad93089910fd2452ea3960afe
OMER DOGU  www.omerdogu.com <evereklili@gmail.com>
2022-04-10 18:41:08 +0300

    Add project files.

---
+++ HelloWorld/Source.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+	//unsigned int x{ 3 };
+	//cout << x << endl;
+	//unsigned int y{ 5 };
+	//cout << (x-y) << endl;
+	float f{ 123456789.0f };
+	std::cout << std::setprecision();
+	std::cout << f << endl;
+
+	return 0;
+}
